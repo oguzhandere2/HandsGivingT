1f9fce45d8182cf7619a6a678a9dde948a64ed00
Ege Hakan Karaağaç <61030767+egehakankar@users.noreply.github.com>
2021-01-01 23:30:25 +0300

    gfhfg

---
+++ app/src/main/java/com/example/handsgivingt/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.handsgivingt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
